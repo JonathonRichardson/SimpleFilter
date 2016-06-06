f7c9794174fd5fa0df11be0a1cfe2b8044eeb203
Jon Richardson <jonathon.m.richardson@gmail.com>
2016-06-06 13:16:27 -0500

    Set up the gulp build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,90 @@
-var gulp = require('gulp');
+var gulp    = require('gulp');
+var fs      = require('fs');
+var _       = require('underscore');
+var wrapper = require('gulp-module-wrapper');
+var insert  = require('gulp-insert');
+var del     = require('del');
+var eol     = require('gulp-eol');
+var gutil   = require('gulp-util');
+var jshint  = require('gulp-jshint');
+var uglify  = require('gulp-uglify');
+var rename  = require('gulp-rename');
+var notify  = require('gulp-notify');
+var concat  = require('gulp-concat');
+var replace = require('gulp-replace');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('default', ['build']);
+
+gulp.task('test', ['build'], function () {
+    console.log("testing...");
 });
 
-gulp.task('test', function () {
-    console.log("testing...");
+gulp.task('clean', function(cb) {
+    del(['dist']);
+    cb();
+});
+
+gulp.task('build', ['clean'], function() {
+    var json = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+    var version = json.version;
+
+    var code    = gulp.src('src/Filter.js').pipe(concat('filter.js'));
+    var amdcode = gulp.src('src/Filter.js').pipe(concat('filter.js'));
+    var cjscode = gulp.src('src/Filter.js').pipe(concat('filter.js'));
+
+    //JSHint
+    gutil.log('Running JSHint');
+    gulp.src('src/Filter.js').pipe(jshint()).pipe(jshint.reporter('default'));
+
+    var pipes = [code, amdcode, cjscode];
+
+    _.each(pipes, function(value) {
+        //Update the version number from the source file
+        value = value.pipe(replace('{{VERSION}}', version));
+    });
+
+    var EOL = '\n';
+
+    var noamd = code
+        .pipe(insert.prepend("var Filter = {};" + EOL + "(function() {" + EOL + EOL))
+        .pipe(insert.append(EOL + EOL + "})();"))
+        .pipe(eol())
+        .pipe(rename('filter-no-amd.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('filter-no-amd.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+
+    var amd = amdcode.pipe(insert.prepend("var Filter = {};" + EOL))
+        .pipe(wrapper({
+            name: false,
+            deps: ['jquery', 'underscore'],
+            args: ['$',      '_'],
+            exports: 'Filter'
+        }))
+        .pipe(eol())
+        .pipe(rename('filter-amd.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('filter-amd.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+
+    var cjs = cjscode.pipe(insert.prepend("var Filter = {};" + EOL))
+        .pipe(wrapper({
+            name: false,
+            deps: ['jquery', 'underscore'],
+            args: ['$',      '_'],
+            exports: 'Filter',
+            type: 'commonjs'
+        }))
+        .pipe(eol())
+        .pipe(rename('filter-commonjs.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('filter-commonjs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+        .pipe(notify({ message: 'Scripts task complete' }));
+
+    return amd;
 });
--- src/Filter.js
+++ src/Filter.js
@@ -1,199 +1,197 @@
-(function() {
-    var parseMomentLookups = {
-        d: "days",
-        t: "days",
-        m: "months",
-        w: "weeks",
-        y: "years"
-    };
+var parseMomentLookups = {
+    d: "days",
+    t: "days",
+    m: "months",
+    w: "weeks",
+    y: "years"
+};
 
-    var parseMoment = function(text) {
-        if (text.match(/^\d\d\d\d$/)) {
-            return moment(text, "YYYY");
+var parseMoment = function(text) {
+    if (text.match(/^\d\d\d\d$/)) {
+        return moment(text, "YYYY");
+    }
+    else if (text.match(/^[tmwy]([+-]\d+)?$/i)) {
+        var key = text.substr(0,1);
+        var number = parseInt(text.substr(1));
+
+        var time = moment().startOf('day');
+
+        if (!_.isNaN(number)) {
+            time.add(number, parseMomentLookups[key.toLowerCase()]);
         }
-        else if (text.match(/^[tmwy]([+-]\d+)?$/i)) {
-            var key = text.substr(0,1);
-            var number = parseInt(text.substr(1));
 
-            var time = moment().startOf('day');
+        return time;
+    }
+    else {
+        return moment(text);
+    }
+};
 
-            if (!_.isNaN(number)) {
-                time.add(number, parseMomentLookups[key.toLowerCase()]);
-            }
+// Set of filters to choose from.  Note that filter and cellValue are assumed to be strings.
+var filters = {
+    substring: function(filter, cellValue, caseInsensitive) {
+        if (caseInsensitive) {
+            return (cellValue.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+        }
+        return (cellValue.indexOf(filter) !== -1);
+    },
+    regex: function(filter, cellValue) {
+        var innerRegex = filter.match(regularExpressionToMatchRegexFilters)[1];
 
-            return time;
+        // Convert to regex object
+        var flags = '';
+        if (filter.match(/i$/)) {
+            flags = 'i';
+        }
+        innerRegex = new RegExp(innerRegex, flags);
+
+        return cellValue.match(innerRegex);
+    },
+    numerical: function(filter, cellValue) {
+        var comparator   = filter.substr(0, 1);
+        var filterNumber = filter.substr(1);
+
+        cellValue = parseFloat(cellValue);
+        filterNumber = parseFloat(filterNumber);
+        if (_.isNaN(filterNumber) || _.isNaN(cellValue)) {
+            return false;
+        }
+
+        if (comparator === ">") {
+            return cellValue > filterNumber;
+        }
+        else if (comparator === "<") {
+            return cellValue < filterNumber;
         }
         else {
-            return moment(text);
+            throw new Error("Failed to match...");
         }
-    };
+    },
+    date: function(filter, cellValue) {
+        var comparator  = filter.substr(1,1); // <, >, or =
+        var date        = parseMoment(filter.substr(2));
+        var cellDate    = parseMoment(cellValue);
+        var granularity = parseMomentLookups[filter.substr(0,1).toLowerCase()];
 
-    // Set of filters to choose from.  Note that filter and cellValue are assumed to be strings.
-    var filters = {
-        substring: function(filter, cellValue, caseInsensitive) {
-            if (caseInsensitive) {
-                return (cellValue.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
-            }
-            return (cellValue.indexOf(filter) !== -1);
-        },
-        regex: function(filter, cellValue) {
-            var innerRegex = filter.match(regularExpressionToMatchRegexFilters)[1];
-
-            // Convert to regex object
-            var flags = '';
-            if (filter.match(/i$/)) {
-                flags = 'i'
-            }
-            innerRegex = new RegExp(innerRegex, flags);
-
-            return cellValue.match(innerRegex);
-        },
-        numerical: function(filter, cellValue) {
-            var comparator   = filter.substr(0, 1);
-            var filterNumber = filter.substr(1);
-
-            cellValue = parseFloat(cellValue);
-            filterNumber = parseFloat(filterNumber);
-            if (_.isNaN(filterNumber) || _.isNaN(cellValue)) {
-                return false;
-            }
-
-            if (comparator === ">") {
-                return cellValue > filterNumber;
-            }
-            else if (comparator === "<") {
-                return cellValue < filterNumber;
-            }
-            else {
-                throw new Error("Failed to match...")
-            }
-        },
-        date: function(filter, cellValue) {
-            var comparator  = filter.substr(1,1); // <, >, or =
-            var date        = parseMoment(filter.substr(2));
-            var cellDate    = parseMoment(cellValue);
-            var granularity = parseMomentLookups[filter.substr(0,1).toLowerCase()];
-
-            if (comparator === ">") {
-                return cellDate.isAfter(date, granularity);
-            }
-            else if (comparator === "<") {
-                return cellDate.isBefore(date, granularity);
-            }
-            else if (comparator === "=") {
-                return cellDate.isSame(date, granularity);
-            }
-            else {
-                throw new Error("Failed to match...")
-            }
+        if (comparator === ">") {
+            return cellDate.isAfter(date, granularity);
         }
-    };
-    $.each(filters, function(key, value) {
-        value.filterName = key;
+        else if (comparator === "<") {
+            return cellDate.isBefore(date, granularity);
+        }
+        else if (comparator === "=") {
+            return cellDate.isSame(date, granularity);
+        }
+        else {
+            throw new Error("Failed to match...");
+        }
+    }
+};
+$.each(filters, function(key, value) {
+    value.filterName = key;
+});
+
+var regularExpressionToMatchRegexFilters = /^\/(.*)\/i?$/;
+
+var filterIsUnnecessary = function(filter) {
+    if (_.isUndefined(filter) || (filter === null)) {
+        return true;
+    }
+
+    // Look for and remove any leading negation marker
+    if (filter.substr(0, 1) == "!") {
+        filter = filter.substr(1);
+    }
+
+    // Now, look for a date signifier
+    if (_.includes("dwmy", filter.substr(0,1))) {
+        filter = filter.substr(1);
+    }
+
+    // Now, look for comparators
+    if ( filter.match(/^[=<>]/) ) {
+        filter = filter.substr(1);
+    }
+
+    if (filter === "") {
+        return true;
+    }
+    // We may have a regex
+    else if (filter.match(/^\//)) {
+        // Until the regex is complete, there is no need to apply the filter.
+        if (!filter.match(regularExpressionToMatchRegexFilters)) {
+            return true;
+        }
+
+        var innerRegex = filter.match(regularExpressionToMatchRegexFilters)[1];
+        var returnVal;
+        try {
+            innerRegex = new RegExp(innerRegex);
+            returnVal = false;
+        }
+        catch(e) {
+            returnVal = true;
+        }
+
+        return returnVal;
+    }
+    return false;
+};
+
+Filter.applyFilter = function(filter, cellValue, caseInsensitive) {
+    // TODO: applyFilter gets called for every row, so this split is happening (n-1) times more than it really
+    // needs to.  Although this hasn't yet caused a problem, it would be prudent to break out the "instantiation"
+    // of the filter, which would also allow us to make other more performant and targeted filters.
+    var innerFilters = filter.split(";");
+    var valueHasFailedFilterTest = false;
+
+    $.each(innerFilters, function(i, filter) {
+        if (!valueMatchesFilterInner(filter, cellValue, caseInsensitive)) {
+            valueHasFailedFilterTest = true;
+            return false;
+        }
     });
 
-    var regularExpressionToMatchRegexFilters = /^\/(.*)\/i?$/;
+    return !valueHasFailedFilterTest;
+};
 
-    var filterIsUnnecessary = function(filter) {
-        if (_.isUndefined(filter) || (filter === null)) {
-            return true;
-        }
+var determineFilter = function(filter) {
+    // Regex
+    if (filter.match(regularExpressionToMatchRegexFilters)) {
+        return filters.regex;
+    }
+    else if (filter.match(/^[<>]/)) {
+        return filters.numerical;
+    }
+    else if (filter.match(/^[dwmy][=<>]/)) {
+        return filters.date;
+    }
+    // DEFAULT: Substring Match
+    else {
+        return filters.substring;
+    }
+};
 
-        // Look for and remove any leading negation marker
-        if (filter.substr(0, 1) == "!") {
-            filter = filter.substr(1);
-        }
+var valueMatchesFilterInner = function(filter, cellValue, caseInsensitive) {
+    var negateAnswer  = false;
 
-        // Now, look for a date signifier
-        if (_.includes("dwmy", filter.substr(0,1))) {
-            filter = filter.substr(1);
-        }
+    // Ensure we have something to filter on, and that it's a string.
+    if (filterIsUnnecessary(filter)) {
+        return true;
+    }
+    filter = filter.toString();
 
-        // Now, look for comparators
-        if ( filter.match(/^[=<>]/) ) {
-            filter = filter.substr(1);
-        }
+    // Ensure that cellValue is a string.
+    cellValue = (_.isUndefined(cellValue) || (cellValue === null) ) ? "" : cellValue.toString();
 
-        if (filter === "") {
-            return true;
-        }
-        // We may have a regex
-        else if (filter.match(/^\//)) {
-            // Until the regex is complete, there is no need to apply the filter.
-            if (!filter.match(regularExpressionToMatchRegexFilters)) {
-                return true;
-            }
+    // Apply negation rule.
+    if (filter.match(/^!/)) {
+        negateAnswer = true;
+        filter = filter.substring(1);
+    }
 
-            var innerRegex = filter.match(regularExpressionToMatchRegexFilters)[1];
-            var returnVal;
-            try {
-                innerRegex = new RegExp(innerRegex);
-                returnVal = false;
-            }
-            catch(e) {
-                returnVal = true;
-            }
+    var filterFunction = determineFilter(filter);
+    var matchesFilter = filterFunction.call(undefined, filter, cellValue, caseInsensitive);
 
-            return returnVal;
-        }
-        return false;
-    };
-
-    var applyFilter = function(filter, cellValue, caseInsensitive) {
-        // TODO: applyFilter gets called for every row, so this split is happening (n-1) times more than it really
-        // needs to.  Although this hasn't yet caused a problem, it would be prudent to break out the "instantiation"
-        // of the filter, which would also allow us to make other more performant and targeted filters.
-        var innerFilters = filter.split(";");
-        var valueHasFailedFilterTest = false;
-
-        $.each(innerFilters, function(i, filter) {
-            if (!valueMatchesFilterInner(filter, cellValue, caseInsensitive)) {
-                valueHasFailedFilterTest = true;
-                return false;
-            }
-        });
-
-        return !valueHasFailedFilterTest;
-    };
-
-    var determineFilter = function(filter) {
-        // Regex
-        if (filter.match(regularExpressionToMatchRegexFilters)) {
-            return filters.regex;
-        }
-        else if (filter.match(/^[<>]/)) {
-            return filters.numerical;
-        }
-        else if (filter.match(/^[dwmy][=<>]/)) {
-            return filters.date;
-        }
-        // DEFAULT: Substring Match
-        else {
-            return filters.substring;
-        }
-    };
-
-    var valueMatchesFilterInner = function(filter, cellValue, caseInsensitive) {
-        var negateAnswer  = false;
-
-        // Ensure we have something to filter on, and that it's a string.
-        if (filterIsUnnecessary(filter)) {
-            return true;
-        }
-        filter = filter.toString();
-
-        // Ensure that cellValue is a string.
-        cellValue = (_.isUndefined(cellValue) || (cellValue === null) ) ? "" : cellValue.toString();
-
-        // Apply negation rule.
-        if (filter.match(/^!/)) {
-            negateAnswer = true;
-            filter = filter.substring(1);
-        }
-
-        var filterFunction = determineFilter(filter);
-        var matchesFilter = filterFunction.call(undefined, filter, cellValue, caseInsensitive);
-
-        return negateAnswer ? !matchesFilter : matchesFilter;
-    };
-})();
+    return negateAnswer ? !matchesFilter : matchesFilter;
+};
