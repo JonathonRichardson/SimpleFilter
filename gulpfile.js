0d4f5876e821cdda3849d2f946e11f631d39d8f1
Jon Richardson <jonathon.m.richardson@gmail.com>
2016-06-06 17:37:36 -0500

    Factor out a lot of the duplicated code.

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,9 @@ var notify  = require('gulp-notify');
 var concat  = require('gulp-concat');
 var replace = require('gulp-replace');
 var jasmine = require('gulp-jasmine');
+var clone   = require('gulp-clone');
 var JasmineConsoleReporter = require('jasmine-console-reporter');
+var merge   = require('merge2');
 
 gulp.task('default', ['build']);
 
@@ -31,67 +33,72 @@ gulp.task('clean', function(cb) {
     });
 });
 
+var wrap = function(stream, type, dependencies, exportName) {
+    var wrapperConfig = {
+        name: false,
+        deps: _.keys(dependencies),
+        args: _.values(dependencies),
+        exports: exportName
+    };
+    
+    return stream.pipe(clone())
+        .pipe(wrapper(_.extend(wrapperConfig, {type: type})))
+        .pipe(eol())
+        .pipe(rename(function(path) { path.basename += '-' + type; }));
+};
+
+var finalize = function(stream, dependencies, exportName) {
+    dependencies = dependencies || {};
+    var EOL = '\n';
+
+    var globalcode = stream.pipe(clone());
+    var amdcode    = stream.pipe(clone());
+    var cjscode    = stream.pipe(clone());
+
+    // Wrap the global code
+    globalcode = globalcode
+        .pipe(insert.prepend("(function() {" + EOL + EOL))
+        .pipe(insert.append(EOL + EOL + "})();"));
+    
+    _.each([globalcode, amdcode, cjscode], function(stream) {
+        stream = stream.pipe(insert.prepend("var " + exportName + " = {};" + EOL));
+    });
+    
+    globalcode = globalcode
+        .pipe(eol())
+        .pipe(rename(function(path) { path.basename += '-globalvar'; }));
+    
+    amdcode = wrap(amdcode, 'amd',      dependencies, exportName);
+    cjscode = wrap(cjscode, 'commonjs', dependencies, exportName);
+    
+    var collectedStreams = merge();
+    _.each([globalcode, cjscode, amdcode], function(stream) {
+        collectedStreams.add(stream.pipe(clone()));
+    });
+
+    return collectedStreams;
+};
+
 gulp.task('build', ['clean'], function() {
     var json = JSON.parse(fs.readFileSync('package.json', 'utf8'));
     var version = json.version;
 
     var code    = gulp.src('src/Filter.js').pipe(concat('filter.js'));
-    var amdcode = gulp.src('src/Filter.js').pipe(concat('filter.js'));
-    var cjscode = gulp.src('src/Filter.js').pipe(concat('filter.js'));
 
     //JSHint
     gutil.log('Running JSHint');
-    gulp.src('src/Filter.js').pipe(jshint()).pipe(jshint.reporter('default'));
+    code = code.pipe(jshint()).pipe(jshint.reporter('default'));
 
-    var pipes = [code, amdcode, cjscode];
+    var finalFiles = finalize(code, {
+        'moment':     'moment',
+        'underscore': '_'
+    }, 'Filter');
 
-    _.each(pipes, function(value) {
-        //Update the version number from the source file
-        value = value.pipe(replace('{{VERSION}}', version));
-    });
+    var minifiedFiles = finalFiles.pipe(clone())
+        .pipe(rename(function(path) { path.basename += '-min'; }))
+        .pipe(uglify());
 
-    var EOL = '\n';
+    var allFiles = merge(finalFiles, minifiedFiles);
 
-    var noamd = code
-        .pipe(insert.prepend("var Filter = {};" + EOL + "(function() {" + EOL + EOL))
-        .pipe(insert.append(EOL + EOL + "})();"))
-        .pipe(eol())
-        .pipe(rename('filter-no-amd.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('filter-no-amd.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
-
-    var amd = amdcode.pipe(insert.prepend("var Filter = {};" + EOL))
-        .pipe(wrapper({
-            name: false,
-            deps: ['jquery', 'underscore', 'moment'],
-            args: ['$',      '_',          'moment'],
-            exports: 'Filter'
-        }))
-        .pipe(eol())
-        .pipe(rename('filter-amd.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('filter-amd.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'))
-        .pipe(notify({ message: 'Scripts task complete' }));
-
-    var cjs = cjscode.pipe(insert.prepend("var Filter = {};" + EOL))
-        .pipe(wrapper({
-            name: false,
-            deps: ['jquery', 'underscore', 'moment'],
-            args: ['$',      '_',          'moment'],
-            exports: 'Filter',
-            type: 'commonjs'
-        }))
-        .pipe(eol())
-        .pipe(rename('filter-commonjs.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('filter-commonjs.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'))
-        .pipe(notify({ message: 'Scripts task complete' }));
-
-    return cjs;
+    return allFiles.pipe(gulp.dest('dist'));
 });
